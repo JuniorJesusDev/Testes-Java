57c1a9b4713e8e544bb41bff4f7a92d1ec12be2b
Junior Jesus <juniorjrg@hotmail.com>
2023-11-05 13:45:07 -0300

    refactor: alter cod for class

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,6 +2,15 @@ public class MinhaClasse {
     
     public static void main(String[] args) {
         
-        System.out.println("Teste");
+        String primeiroNome = "Junior";
+        String segundoNome = "Jesus";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
